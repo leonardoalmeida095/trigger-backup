5c4f198c02d73a2745fca66ffca4a0125661c55d
Leonardo Almeida <61922601+leonardoalmeida095@users.noreply.github.com>
2020-06-15 18:59:38 -0300

    Add files via upload

---
+++ utg_moverImvDel.sql
@@ -0,0 +1,17 @@
+--Criação do gatilho "utg_moverImvDel" para backup de dados deletados da tabela "imovel" para "imovelDel"
+
+CREATE TRIGGER utg_moverImvDel
+	ON imovel AFTER DELETE AS
+	BEGIN
+		DECLARE @COD INT, @TIPO VARCHAR(30), @AREA FLOAT, @VALOR FLOAT, @CODCLI INT
+
+		SELECT @COD = codImovel, @TIPO = tipo, @AREA = area, 
+			@VALOR = valor, @CODCLI = cod_cliente FROM deleted
+
+		INSERT INTO imovelDel(codImovel, tipo, area, valor, cod_cliente) 
+			VALUES(@COD, @TIPO, @AREA, @VALOR, @CODCLI)
+		
+	END
+GO
+
+
